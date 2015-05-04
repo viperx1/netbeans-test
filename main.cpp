2c07cacf4669ea1ae674abd979f0eb7b47d06d54
viperx1 <328461@gmail.com>
2015-05-04 20:45:10 +0200

    new commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: 32846_000
+ *
+ * Created on 4 maja 2015, 20:43
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
